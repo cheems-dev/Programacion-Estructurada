4dc3951d377c6a9df76f43d721a543f96d1feea2
Davaria <daprimovaria@outlook.com>
2020-01-12 18:35:10 -0500

    Creacion de archivo readme.md y main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main()
+{
+    int integerA = 1;
+    float floatA = 1.35;
+    char charA = 'A';
+    bool boolA = true;
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# <center>Programacion Estructurada</center>
+
+Aprendiendo con el lenguaje de programacion C
